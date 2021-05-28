cddea4789d5ee602cdb6b0b61e2306d080bb317f
tom-harkness <u3171904@uni.canberra.edu.au>
2021-05-29 09:58:33 +1000

    solved problem 8, fixed bug in problem 3

--- problem3.c
+++ problem3.c
@@ -13,7 +13,10 @@ bool isPrime(long int n) {
     if (n == 2) {
         return true;
     }
-    for (long int i = 3; i*i<=n; i+=2) {
+    if (n % 2 == 0) {
+        return false;
+    }
+    for (long int i = 2; i*i<=n; i+=2) {
         if (n % i == 0) {
             return false;
         }

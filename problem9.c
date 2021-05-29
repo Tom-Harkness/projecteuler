2aff548db780e3fd3530dab0a864fa8dc5e9a914
tom-harkness <u3171904@uni.canberra.edu.au>
2021-05-29 10:05:45 +1000

    solved problem 9

---
+++ problem9.c
@@ -0,0 +1,23 @@
+// Project Euler problem 8: https://projecteuler.net/problem=8
+// Author: Tom Harkness
+// May 29, 2021
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#define TARGET_SUM 1000
+
+int main(void) {
+    for (int a = 1; a < TARGET_SUM; a++) {
+        for (int b = 1; b < TARGET_SUM; b++) {
+            for (int c = 1; c < TARGET_SUM; c++) {
+                if (a*a + b*b - c*c == 0) {
+                    if (a + b + c == 1000) {
+                        printf("%d", a*b*c);
+                        return 0;
+                    }
+                }
+            }   
+        }
+    }
+}
